fd71ec577d193935d3356f9a7b99bd9fa8afa29e
seamric <65127867+seamric@users.noreply.github.com>
2020-08-04 06:44:29 +0530

    This is the main program
    This main file executes the whole program

---
+++ main.py
@@ -0,0 +1,13 @@
+height = float(input('Enter height for tent (in meters) : '))
+radius = float(input('Enter radius for tent (in meters) : '))
+sl_height = float(input('Enter slant height for tent (in meters) : '))
+pi = 3.14
+price_per_unit = 0.5
+
+total_surface_area_cylinder = 2 * pi * radius * (radius + height)
+total_surface_area_cone = pi * radius * (radius + sl_height)
+total_surface_area_tent = total_surface_area_cylinder + total_surface_area_cone
+total_price_exclusive_tax = (price_per_unit * total_surface_area_tent)
+total_tax = 18 / 100 * total_price_exclusive_tax
+total_price_inclusive_tax = total_price_exclusive_tax + total_tax
+print('Price for this tent is : $', total_price_inclusive_tax)
