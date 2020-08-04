f298a870e1ac1880c416d6f7ae4da68ec37a931b
seamric <65127867+seamric@users.noreply.github.com>
2020-08-04 06:58:56 +0530

    Using math module
    Using the pi variable from math module gives much accurate results

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-height = float(input('Enter height for tent (in meters) : ')) # Takes in the height entered by user
-radius = float(input('Enter radius for tent (in meters) : ')) # Takes in the radius entered by user
-sl_height = float(input('Enter slant height for tent (in meters) : ')) # Takes in the slant height of cone entered by user
-pi = 3.14
-price_per_unit = 0.5 # Price per unit set by the company
+import math
+height = float(input('Enter height for tent (in meters) : '))
+radius = float(input('Enter radius for tent (in meters) : '))
+sl_height = float(input('Enter slant height for tent (in meters) : '))
+price_per_unit = 0.5
 
-total_surface_area_cylinder = 2 * pi * radius * (radius + height)
-total_surface_area_cone = pi * radius * (radius + sl_height)
+total_surface_area_cylinder = 2 * math.pi * radius * (radius + height)
+total_surface_area_cone = math.pi * radius * (radius + sl_height)
 total_surface_area_tent = total_surface_area_cylinder + total_surface_area_cone
 total_price_exclusive_tax = (price_per_unit * total_surface_area_tent)
 total_tax = 18 / 100 * total_price_exclusive_tax
